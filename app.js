613968741f5958bea35e041d061851c71a2f31a7
w4721x <patoliyaraj@gmail.com>
2018-10-23 17:51:10 +0530

    changed app.js

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+// Index Route
+app.get("/", (req, res) => {
+  res.send("Hello From index page...");
+});
+
+// About Route
+app.get("/about", (req, res) => {
+  console.log("in about");
+});
+
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
